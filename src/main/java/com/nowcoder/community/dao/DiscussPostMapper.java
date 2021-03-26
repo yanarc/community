9b65039a987898d4b600a807f359c867a94e40c6
yanarc <1432374934@qq.com>
2021-03-26 22:39:04 +0800

    first commit

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
